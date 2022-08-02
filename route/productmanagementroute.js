7b58fd166e10ba2892a187bba855c3fcd5d29bf6
WebDevolper1122 <sb2137842@gmail.com>
2022-08-02 12:42:17 +0500

    commit

--- route/productmanagementroute.js
+++ route/productmanagementroute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Router = express.Router();//express function router call with (.) 
 
+
 // Calling The Controller 4th step
 const { ProductData,
       GetProductData,
@@ -8,7 +9,17 @@ const { ProductData,
       DeleteProductData
 }= require("../controller/productmanagementcontroll")
 
-Router.post("/ProductData",ProductData);
+// Calling The Controller 4th step
+
+
+// Calling My MiddleWares
+const {
+    check
+}=require('../middle-wares/FirstMiddleWare'); //Now We Can Use MiddleWare (🚉🚉🚉) Where u Want 
+
+// Calling My MiddleWares  
+
+Router.post("/ProductData",check,ProductData); // I Use MiddleWare In this Route
 Router.get("/GetProductData",GetProductData)
 Router.post("/UpDateProductData",UpDateProductData) //We can Use Put But Sometimes We Send Payload So That's Why I  Use Post
 Router.delete("/DeleteProductData",DeleteProductData)
