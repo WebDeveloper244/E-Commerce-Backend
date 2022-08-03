1d17c433bd219c1f9d6702e8af219ed4e26f0526
WebDevolper1122 <sb2137842@gmail.com>
2022-08-03 12:51:43 +0500

    commit

--- controller/productmanagementcontroll.js
+++ controller/productmanagementcontroll.js
@@ -9,7 +9,7 @@ try {
      const DocToCreate = new ProductModel({
         ProductName,
         ProductPrice,
-        ImageUrl: `/assets/Product/${req.file.filename}`,//req.file.filename
+        ImageUrl: `/assets/Product/${req.file.filename}`,
         ImageName: req.file.originalname,
         ImageMimeType: req.file.mimetype,
         
