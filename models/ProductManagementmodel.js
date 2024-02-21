0d98ee0eab41b4a93816c5727475d90dbcc642b0
WebDeveloper244 <sb2137842@gmailcom>
2024-02-21 23:43:01 +0500

    add

--- models/ProductManagementmodel.js
+++ models/ProductManagementmodel.js
@@ -23,6 +23,7 @@ const ProductSchema = mongoose.Schema({
     category:{type:String,required:true},
     softDelete:{type:Number,default:0},
     Status :{ type:Number , default:1},
+    isEdit :{ type:Boolean , default:false},
     CreateDate :{
         type:String,
         default:`${day}-${month}-${year}-${time}`
